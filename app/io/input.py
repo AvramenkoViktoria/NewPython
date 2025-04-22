71de363ece63732e704de6ec2147d746dfcd8e1d
just-belive-in-unicorn <anastasiyadernova2608@gmail.com>
2025-04-22 23:05:27 +0300

    Added functions for file reading and writing

---
+++ app/io/input.py
@@ -0,0 +1,21 @@
+def input_from_console():
+    """
+    Функція для вводу тексту з консолі
+    """
+    pass
+
+
+def input_from_file_builtin(filepath):
+    """
+    Функція для зчитування з файлу за допомогою вбудованих можливостей Python
+    :param filepath: шлях до файлу
+    """
+    pass
+
+
+def input_from_file_pandas(filepath):
+    """
+    Функція для зчитування з файлу за допомогою бібліотеки pandas
+    :param filepath: шлях до файлу
+    """
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,15 @@
+def output_to_console(text):
+    """
+    Функція для виводу тексту у консоль
+    :param text: текст для виводу
+    """
+    pass
+
+
+def output_to_file_builtin(text, filepath):
+    """
+    Функція для запису тексту у файл за допомогою вбудованих можливостей Python
+    :param text: текст для запису
+    :param filepath: шлях до файлу
+    """
+    pass
